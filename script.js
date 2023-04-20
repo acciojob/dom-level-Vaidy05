883f5a022ba75af3cea0e3ec8555944f2966a0a8
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-20 13:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let arr = document.querySelector("ul");
+let s = arr.children;
+for(let i=0;i<s.length;i++){
+	if(s[i].id=="level"){
+		alert("The level of the element is: "+(i+1));
+		break;
+	}
+}
